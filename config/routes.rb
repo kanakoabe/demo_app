fd9e0c1db20ead11586121bea7cae4a28184de51
kanakoabe <3nnrkheg.kana@ozzio.jp>
2018-03-08 12:10:07 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top_page' => 'root#top_page'
+  #getは画面が変わりますというHTTPメソッドの一つ。
+end
